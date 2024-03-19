be44160b80afd84135ffcf84e7105b10163e8dad
adenita <nittaluta@gmail.com>
2024-03-19 05:17:01 +0100

    added authentication models

---
+++ src/app/shared/models/authentication.ts
@@ -0,0 +1,19 @@
+import {Role} from "./user";
+
+export interface RegisterTransport {
+  name: string;
+  username: string;
+  password: string;
+  role: Role;
+}
+
+export interface LoginTransport {
+  username: string;
+  password: string;
+}
+
+export interface TokenTransport {
+  token: string;
+  username: string;
+  role: Role;
+}
