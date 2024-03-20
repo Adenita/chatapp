f78d98789042bee7168a91ec63b1978b32a5bfec
adenita <nittaluta@gmail.com>
2024-03-20 05:55:47 +0100

    added getByUsername method

--- src/app/core/services/http/user.service.ts
+++ src/app/core/services/http/user.service.ts
@@ -2,6 +2,7 @@ import {DataService} from "./data.service";
 import {UserListTransport, UserTransport} from "../../../shared/models/user";
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +11,10 @@ export class UserService extends DataService<UserTransport, UserListTransport> {
   constructor(private httpClient: HttpClient) {
     super(httpClient);
     this.apiUrl = 'users';
+  }
+
+
+  getByUsername(username: string): Observable<UserTransport> {
+    return this.httpClient.get<UserTransport>(`${this.url}/${this.apiUrl}/${username}$`);
   }
 }
