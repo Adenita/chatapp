46f7ee15ec80dac6a6b2e4a27e3eb8e730fd7fae
adenita <nittaluta@gmail.com>
2024-03-23 08:58:09 +0100

    added room module

---
+++ src/app/modules/room/room.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from "@angular/core";
+import {MessageComponent} from "./components/message/message.component";
+import {RoomComponent} from "./pages/room/room.component";
+import {RoomListModalComponent} from "./components/room-list-modal/room-list-modal.component";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {SharedModule} from "../../shared/shared.module";
+import {RoomRoutingModule} from "./room-routing.module";
+
+@NgModule({
+  declarations: [
+    MessageComponent,
+    RoomComponent,
+    RoomListModalComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    RoomRoutingModule,
+    SharedModule
+  ],
+  bootstrap: []
+})
+export class RoomModule { }
