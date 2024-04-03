11cad2d86a912d78bb0a6fb994ee77e5c3782cc7
adenita <nittaluta@gmail.com>
2024-04-03 07:18:26 +0200

    refactored message component

--- src/app/modules/room/components/message/message.component.ts
+++ src/app/modules/room/components/message/message.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import {MessageTransport} from "../../../../shared/models/message";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-message',
@@ -6,6 +8,13 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./message.component.sass']
 })
 export class MessageComponent {
-  @Input() content!: string;
   @Input() isSender!: boolean;
+  @Input() message!: MessageTransport
+  dateFormat: string = 'hh:mm - EEEE, MMMM d Y';
+
+  constructor(private datePipe: DatePipe) {}
+
+  formatDate(date: Date) {
+    return this.datePipe.transform(date, this.dateFormat);
+  }
 }
